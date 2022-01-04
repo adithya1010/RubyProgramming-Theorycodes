3081d1db2cd84dcfbba4b5a136d527c250276ce2
Adithya S.T <57008821+adithya1010@users.noreply.github.com>
2022-01-04 20:13:08 +0530

    Intro to Ruby

---
+++ Intro.rb
@@ -0,0 +1,23 @@
+#
+
+puts "demo on blocks"
+
+BEGIN{ puts "begin block" }
+END{ puts "end block" }
+
+#print function
+print <<"str"
+this is string
+ruby
+program
+str
+
+#string concatenation
+a="sample"
+b="program"
+puts a+b
+
+#addition
+a=64
+b=72
+puts a+b
